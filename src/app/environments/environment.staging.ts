4778039eeca46ba48802c6481d5bdedbb3f3e993
Megue14 <75349919+Megue14@users.noreply.github.com>
2023-02-05 17:58:48 -0500

    Config

---
+++ src/app/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_URL: 'http://activemodebackend-env.eba-sutbdvmi.us-east-1.elasticbeanstalk.com/api',
+  PK_KEY: 'pk_ff59e195efc442ecbe0f22151114e22c',
+  ID: 'mkvbujzmcmpjymbuozat'
+};
