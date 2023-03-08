7d5504bbc0887038265631af5ae540109c06124f
Jonathan Cardenas <greiff.jonathan@gmail.com>
2023-03-08 16:34:34 -0500

    servicio de dia grupo

--- src/app/core/http/z_dia_grupo/z-dia-grupo.service.ts
+++ src/app/core/http/z_dia_grupo/z-dia-grupo.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZDiaGrupoService {
 
-  constructor() { }
+  apiURL = environment.API_URL
+
+  constructor(private http: HttpClient) { }
+
+  getDiasGrupoParam(){
+    const url = this.apiURL + "/dias-grupo/param"
+    return this.http.get<any>(url)
+  }
+
+  getDiaGrupoParam(id:number){
+    const url = this.apiURL + "/dia-grupo/param"
+    return this.http.get<any>(url,{params:{id:id}})
+  }
+
+  createDiaGrupoParam(nombre:string,valor:string,idDia:number){
+    const url = this.apiURL + "/dia-grupo/param"
+    const data = {nombre:nombre,valor:valor,idDia:idDia}
+    return this.http.post<any>(url,data)
+  }
+
+  updateDiaGrupoParam(nombre:string){
+    const url = this.apiURL + "/dia-grupo/param/update"
+    const data = {nombre:nombre}
+    return this.http.put<any>(url,data)
+  }
+
+  changeStateDiaGrupoParam(id:number){
+    const url = this.apiURL + "/dia-grupo/param/state"
+    const data = {id:id}
+    return this.http.put<any>(url,data)
+  }
 }
