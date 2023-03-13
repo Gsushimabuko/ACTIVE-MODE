16ef05ca57969070195733cec6ac650b6ada43ae
Gabriel Shimabuko <20171470@aloe.ulima.edu.pe>
2023-03-13 00:14:16 -0500

    reporte diario de alumnos

--- src/app/core/http/z_matricula/z-matricula.service.ts
+++ src/app/core/http/z_matricula/z-matricula.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZMatriculaService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+  
+  
+  constructor(private http: HttpClient) {}
+
+
+  getMatriculadosReporte(dia: number, mes: number, ano:number): Observable<Usuario[]>  {
+    return this.http.get<Usuario[]>(this.API_URL + '/test-funcion',{params:{mes:mes, dia:dia, ano:ano}})
+  }
+
+  getFullYear2023() :  Observable<any[]> {
+    return this.http.get<any[]>('https://calendar-json-api.up.railway.app/fullyear')
+  }
 }
--- src/app/modules/admin/alumnos/alumnos.component.ts
+++ src/app/modules/admin/alumnos/alumnos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ZMatriculaService } from '../../../core/http/z_matricula/z-matricula.service';
 
 @Component({
   selector: 'app-alumnos',
@@ -6,5 +7,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./alumnos.component.css']
 })
 export class AlumnosComponent {
-  
+  Object = Object;
+  reporte! : any[]
+  calendario!: any
+  calendarioMes!:any
+  mesSeleccionado!:string
+  loader!:boolean
+  fechaBuscada!:string
+
+  constructor(private matriculadosService: ZMatriculaService){
+    this.cargarCalendario2023();
+    
+  }
+
+  cargarCalendario2023(){
+    this.loader = true
+    this.matriculadosService.getFullYear2023().subscribe(res => {
+      this.calendario = res
+     
+      this.loader = false
+
+    })
+  }
+  buscarResultadosDia(dia:number){
+    this.loader = true
+    let codMes
+    console.log("Mes Seleccionado: ", this.mesSeleccionado)
+    if(this.mesSeleccionado == "January"){
+      codMes=1
+    }else if (this.mesSeleccionado == "February"){
+      codMes=2
+    }else if (this.mesSeleccionado == "March"){
+      codMes=3
+    }else if (this.mesSeleccionado == "April"){
+      codMes=4
+    }else if (this.mesSeleccionado == "May"){
+      codMes=5
+    }else if (this.mesSeleccionado == "June"){
+      codMes=6
+    }else if (this.mesSeleccionado == "July"){
+      codMes=7
+    }else if (this.mesSeleccionado == "August"){
+      codMes=8
+    }else if (this.mesSeleccionado == "September"){
+      codMes=9
+    }else if (this.mesSeleccionado == "October"){
+      codMes=10
+    }else if (this.mesSeleccionado == "November"){
+      codMes=11
+    }else if (this.mesSeleccionado == "December"){
+      codMes=12
+    }else{
+      codMes=0
+    }
+    console.log("DIA: ", dia, "MES: ", codMes)
+    
+    this.matriculadosService.getMatriculadosReporte(dia,codMes,2023).subscribe((res) => {
+      this.reporte = res
+      this.loader = false
+      this.fechaBuscada = dia.toString()+ " " + this.mesSeleccionado + "  " + '2023'
+      })
+  }
+  cambiarMes(mesNombreEvt:any){
+    const mes = this.calendario[mesNombreEvt.value]
+    this.mesSeleccionado = mesNombreEvt.value
+    const s1 = mes[0]
+    const s2 = mes[1]
+    const s3 = mes[2]
+    const s4 = mes[3]
+    this.calendarioMes = {
+      semana1: s1,
+      semana2: s2,
+      semana3: s3,
+      semana4: s4
+    }
+  }
 }
